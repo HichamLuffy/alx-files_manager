b9cd84906c1ccaafb6b1fd3950f3de9c0b5e3711
HichamLuffy <imalexvssteve2020@gmail.com>
2024-06-16 13:33:20 +0100

    creating files + first task

---
+++ alx-files_manager/utils/redis.js
@@ -0,0 +1,57 @@
+const redis = require('redis');
+const { promisify } = require('util');
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, _reject) => {
+      this.client.del(key, (error) => {
+        if (error) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
