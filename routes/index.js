f705be7a57175b8fc227f07b3d1f29b34e3c2bd4
HichamLuffy <imalexvssteve2020@gmail.com>
2024-06-20 02:34:31 +0100

    task 2

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,22 @@
-const express = require('express');
-const AppController = require('../controllers/AppController');
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
-
-const UsersController = require('../controllers/UsersController');
 router.post('/users', UsersController.postNew);
-
-const AuthController = require('../controllers/AuthController');
-
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', AuthController.getMe);
-
-const FilesController = require('../controllers/FilesController');
-
+router.get('/users/me', UsersController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 router.get('/files/:id/data', FilesController.getFile);
-
 
 export default router;
